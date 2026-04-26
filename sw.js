5721f2d4c9f69385c814d365b929454be15dcda7
Shams&Dean <shamsetdean@gmail.com>
2026-04-26 11:02:02 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -4,7 +4,7 @@
    Version : 1.0.0
 ═══════════════════════════════════════════════════════ */
 
-const CACHE_NAME = 'vacuum-v1';
+const CACHE_NAME = 'vacuum-v2';
 
 const STATIC_ASSETS = [
   '/vacuum/',
